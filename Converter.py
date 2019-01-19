f0e1445d1ecdcd3e1c466ea67e2dc7b4998c3d2d
dolphinxzhang <46572420+dolphinxzhang@users.noreply.github.com>
2019-01-19 10:03:06 -0800

    Add files via upload

---
+++ Converter.py
@@ -0,0 +1,49 @@
+def DecimalToBinary(decimal):
+    """
+    Converts a decimal number entered by user
+    into the corresponding binary number.
+
+    decimal: type string
+
+    return: type string
+
+    """
+
+        
+    return ''
+
+
+def BinaryToDecimal(binary):
+    """
+    Converts a binary number entered by user
+    into the corresponding decimal number.
+
+    binary: type string
+
+    return: type string
+
+    """
+
+        
+    return ''
+
+
+
+
+if __name__ == '__main__':
+
+    number = raw_input("Enter a decimal number to convert to Binary: ")
+    binary = DecimalToBinary(number)
+
+    print
+    print 'Entered Decimal: %s' % number
+    print 'Binary: %s' % binary
+    print
+    
+    number = raw_input("Enter a binary number to convert to Decimal: ")
+    decimal = BinaryToDecimal(number)
+
+    print
+    print 'Entered Binary: %s' % number
+    print 'Decimal: %s' % decimal
+    print
